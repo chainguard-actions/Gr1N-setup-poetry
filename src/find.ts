d45d73ec0eab2600d8d286088f3ed235eda213bf
actionchain <actionchain@chainguard.dev>
2026-05-09 18:30:11 +0000

    Update Gr1N/setup-poetry@v9

---
+++ src/find.ts
@@ -0,0 +1,42 @@
+import { addPath } from "@actions/core"
+import { exec } from "@actions/exec"
+import { downloadTool } from "@actions/tool-cache"
+import { Inputs } from "./inputs"
+import os from "os"
+import path from "path"
+
+const GET_POETRY_URL = "https://install.python-poetry.org"
+
+export async function findPoetry(inputs: Inputs): Promise<void> {
+  // Download get-poetry.py
+  const getPoetryPath = await downloadTool(GET_POETRY_URL)
+
+  // Run Poetry installation script
+  await exec("python", [getPoetryPath, ...getPoetryInstallArgs(inputs)])
+
+  // Add Poetry executable to the PATH
+  const poetryPath = path.join(os.homedir(), ...getPoetryPathArgs())
+  addPath(poetryPath)
+}
+
+function getPoetryInstallArgs(inputs: Inputs): string[] {
+  const args: string[] = ["--yes"]
+
+  if (inputs.preview) {
+    args.push("--preview")
+  }
+  if (inputs.version) {
+    args.push(`--version=${inputs.version}`)
+  }
+
+  return args
+}
+
+function getPoetryPathArgs(): string[] {
+  switch (os.platform()) {
+    case "win32":
+      return ["AppData", "Roaming", "Python", "Scripts"]
+    default:
+      return [".local", "bin"]
+  }
+}
